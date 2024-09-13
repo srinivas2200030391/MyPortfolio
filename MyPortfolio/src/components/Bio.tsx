0e7416257f922966f45050d980fc01fd5d9cd634
Srinivas <vasus4990@gmail.com>
2024-09-13 20:35:15 +0530

    Bio translateX changed

--- MyPortfolio/src/components/Bio.tsx
+++ MyPortfolio/src/components/Bio.tsx
@@ -4,22 +4,25 @@ export default function Bio() {
   return (
     <div>
       <section className="flex max-w-4xl flex-col gap-12 pt-20" id="bio">
-        <motion.h2 initial={{ opacity: 0, y: -20 }}
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{duration:1}}
+          transition={{ duration: 1 }}
           className="text-center text-3xl lg:text-4xl">
           Bio
         </motion.h2>
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-        transition={{duration:0.8,delay:0.2}}>
+          transition={{ duration: 0.8, delay: 0.2 }}
+          style={{ transform: "translateX(2rem)" }}>
           {BIO.map((bio, index) => (
             <motion.p
               initial={{ opacity: 0, x: -20 }}
               whileInView={{ opacity: 1 }}
-              transition={{duration:0.8,delay:index*0.5}}
-              key={index} className="mb-4 text-lg lg:text-xl">
+              transition={{ duration: 0.8, delay: index * 0.5 }}
+              key={index}
+              className="mb-4 text-lg lg:text-xl">
               {bio}
             </motion.p>
           ))}
